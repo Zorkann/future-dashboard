0ab315a97d3695ee7cba8742333d443f5e805261
Marcin Skowyra <marcinskowyra@Marcins-MacBook-Pro.local>
2024-04-29 23:40:05 +0200

    Fix typscript graph state

--- src/components/DashboardDrawer/DashboardDrawer.tsx
+++ src/components/DashboardDrawer/DashboardDrawer.tsx
@@ -1,30 +1,32 @@
 import { DrawerProps, Drawer } from "../Drawer";
 import { CheckboxButton } from "@components/Button";
 
+type GraphState = {
+  Graph1: boolean;
+  Graph2: boolean;
+};
+
 type DashboardDrawerProps = {
-	graphStates: {
-		Graph1: boolean;
-		Graph2: boolean;
-	};
-	toggleGraphVisibility: (graphName: keyof typeof graphStates) => void;
+  graphStates: GraphState;
+  toggleGraphVisibility: (graphName: keyof GraphState) => void;
 } & DrawerProps;
 export function DashboardDrawer({
-	toggleGraphVisibility,
-	graphStates,
-	...rest
+  toggleGraphVisibility,
+  graphStates,
+  ...rest
 }: DashboardDrawerProps) {
-	return (
-		<Drawer {...rest}>
-			<CheckboxButton
-				label="Graph 1"
-				checked={graphStates.Graph1}
-				onChange={() => toggleGraphVisibility("Graph1")}
-			/>
-			<CheckboxButton
-				label="Graph 2"
-				checked={graphStates.Graph2}
-				onChange={() => toggleGraphVisibility("Graph2")}
-			/>
-		</Drawer>
-	);
+  return (
+    <Drawer {...rest}>
+      <CheckboxButton
+        label="Graph 1"
+        checked={graphStates.Graph1}
+        onChange={() => toggleGraphVisibility("Graph1")}
+      />
+      <CheckboxButton
+        label="Graph 2"
+        checked={graphStates.Graph2}
+        onChange={() => toggleGraphVisibility("Graph2")}
+      />
+    </Drawer>
+  );
 }
