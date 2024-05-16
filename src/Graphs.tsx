6907b026f3e362b97d347b0c6fa2f1076c5a9d7d
Shirai92 <shirai92@gmail.com>
2024-05-16 17:58:03 +0200

    add comand start to package.json to start vite and json server in the same time

--- src/Graphs.tsx
+++ src/Graphs.tsx
@@ -11,66 +11,66 @@ import { Graph10 } from "./Graph10";
 import { useGraphsContext } from "@features/themes";
 
 export function Graphs() {
-  const { graphStates } = useGraphsContext();
+	const { graphStates } = useGraphsContext();
 
-  return (
-    <div className="grid grid-cols-1 grid-rows-[repeat(24,80px)] lg:grid-rows-[repeat(8,80px)] lg:grid-cols-[5fr,4fr,6fr] grid-flow-col gap-8 p-[40px] pt-[104px]">
-      {graphStates.Graph_1 && (
-        <div className="row-span-1">
-          <Graph1 />
-        </div>
-      )}
-      {graphStates.Graph_2 && (
-        <div className="row-span-3">
-          <Graph2 />
-        </div>
-      )}
+	return (
+		<div className="grid grid-cols-1 grid-rows-[repeat(24,80px)] lg:grid-rows-[repeat(8,80px)] lg:grid-cols-[5fr,4fr,6fr] grid-flow-col gap-8 p-[40px] pt-[104px]">
+			{graphStates.Graph_1 && (
+				<div className="row-span-1">
+					<Graph1 />
+				</div>
+			)}
+			{graphStates.Graph_2 && (
+				<div className="row-span-3">
+					<Graph2 />
+				</div>
+			)}
 
-      {graphStates.Graph_3 && (
-        <div className="row-span-2">
-          <Graph3 />
-        </div>
-      )}
-      {graphStates.Graph_4 && (
-        <div className="row-span-2">
-          <Graph4 />
-        </div>
-      )}
-      {graphStates.Graph_5 && (
-        <div className="row-span-2">
-          <Graph5 />
-        </div>
-      )}
-      {graphStates.Graph_5 && (
-        <div className="row-span-1">
-          <Graph5 />
-        </div>
-      )}
-      {graphStates.Graph_6 && (
-        <div className="row-span-3">
-          <Graph6 />
-        </div>
-      )}
-      {graphStates.Graph_7 && (
-        <div className="row-span-2">
-          <Graph7 />
-        </div>
-      )}
-      {graphStates.Graph_8 && (
-        <div className="row-span-2">
-          <Graph8 />
-        </div>
-      )}
-      {graphStates.Graph_9 && (
-        <div className="row-span-4">
-          <Graph9 />
-        </div>
-      )}
-      {graphStates.Graph_10 && (
-        <div className="row-span-2">
-          <Graph10 />
-        </div>
-      )}
-    </div>
-  );
+			{graphStates.Graph_3 && (
+				<div className="row-span-2">
+					<Graph3 />
+				</div>
+			)}
+			{graphStates.Graph_4 && (
+				<div className="row-span-2">
+					<Graph4 />
+				</div>
+			)}
+			{graphStates.Graph_5 && (
+				<div className="row-span-2">
+					<Graph5 />
+				</div>
+			)}
+			{graphStates.Graph_5 && (
+				<div className="row-span-1">
+					<Graph5 />
+				</div>
+			)}
+			{graphStates.Graph_6 && (
+				<div className="row-span-3">
+					<Graph6 />
+				</div>
+			)}
+			{graphStates.Graph_7 && (
+				<div className="row-span-2">
+					<Graph7 />
+				</div>
+			)}
+			{graphStates.Graph_8 && (
+				<div className="row-span-2">
+					<Graph8 />
+				</div>
+			)}
+			{graphStates.Graph_9 && (
+				<div className="row-span-4">
+					<Graph9 />
+				</div>
+			)}
+			{graphStates.Graph_10 && (
+				<div className="row-span-2">
+					<Graph10 />
+				</div>
+			)}
+		</div>
+	);
 }
