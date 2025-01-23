a8f7b20815ff98c7a8862100c09b84c948f6e301
Shirai92 <shirai92@gmail.com>
2025-01-23 22:29:48 +0100

    add react-hook-form, devtools, state,renders

--- src/LoginPage2/LoginPage2.tsx
+++ src/LoginPage2/LoginPage2.tsx
@@ -1,5 +1,67 @@
-import React from 'react';
+import { useForm } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
+
+type FormValues = {
+  username: string;
+  email: string;
+};
 
 export const LoginPage2 = () => {
-  return <div>LoginPage2</div>;
+  const form = useForm<FormValues>();
+  const { register, control, handleSubmit } = form;
+
+  const onSubmit = (data: FormValues) => {
+    console.log('Form submmited', data);
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="w-full max-w-xs">
+        <h2 className="text-center text-2xl font-bold mb-6">LoginPage2</h2>
+        <form
+          className="bg-black shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+        >
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline bg-gray-800"
+              type="text"
+              id="username"
+              {...register('username', { required: 'Username is required' })}
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline bg-gray-800"
+              type="email"
+              id="email"
+              {...register('email', { required: 'Email is required' })}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+        <DevTool control={control} />
+      </div>
+    </div>
+  );
 };
